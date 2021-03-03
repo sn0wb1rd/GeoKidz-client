716b273b879ea0a748398ee134985f625c19c87a
Alexandra Westendorp <alexandrawestendorp@gmail.com>
2021-03-03 18:56:09 +0100

    style background, logo and signup page

--- src/App.js
+++ src/App.js
@@ -1,58 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Switch, Route, useParams } from "react-router-dom";
-import axios from 'axios';
-import config from './config'
-import Home from "./components/Home"
+import axios from "axios";
+import config from "./config";
+import Home from "./components/Home";
 import SignIn from "./components/SingIn";
 import SignUp from "./components/SignUp";
 import Nav from "./components/Nav";
 
 //test
 
-
 function App() {
-    const [loggedInUser, setLoggedInUser]  = useState(null)
-    const [error, setError]  = useState(null)
-    
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [error, setError] = useState(null);
 
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    let user = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
 
-const handleSignUp = (event) => {
-  event.preventDefault()
-  let user = {
-    username: event.target.username.value,
-    password: event.target.password.value,
-  }
-
-  axios
-    .post(`${config.API_URL}/api/signup`,user)
-    .then((response) => {
-      setLoggedInUser(response.data)
-      props.history.push('/')   
-    })
-    .catch((err) => {
-      setError(err)
-    })
-
-}
-
+    axios
+      .post(`${config.API_URL}/api/signup`, user)
+      .then((response) => {
+        setLoggedInUser(response.data);
+        props.history.push("/");
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  };
 
   return (
     <div className="App">
-      <Switch>
-      <Nav />
-        <Route exact path="/" component={Home} 
-
-        />
-        <Route path="/signin" component={SignIn}
-
-        />
-        <Route path="/signup" render={
-          (routeProps) => {
-            return <SignUp onSignUp=
-            {handleSignUp} {...routeProps}
-            />
-        }}/>
-      </Switch>
+      <div className="gradient-background">
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signin" component={SignIn} />
+          <Route
+            path="/signup"
+            render={(routeProps) => {
+              return <SignUp onSignUp={handleSignUp} {...routeProps} />;
+            }}
+          />
+        </Switch>
+      </div>
+      <div class="empty-triangle"></div>
     </div>
   );
 }
