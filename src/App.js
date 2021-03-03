c5bfe6f0f4efb143a9eb0c230eb4c9c2d4f92f01
Alexandra Westendorp <alexandrawestendorp@gmail.com>
2021-03-03 17:18:17 +0100

    Components creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Home from "./components/Home"
+import Home from "./components/Home";
+import Nav from "./components/Nav";
 import SignIn from "./components/SingIn";
 import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={Home} 
-
-        />
-        <Route path="/signin" component={SignIn}
-
-        />
-        <Route path="/signup" component={SignUp}
-
-        />
-
-
-      </Switch>
+      <div class="gradient-background">
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+        </Switch>
+      </div>
+      <div class="empty-triangle"></div>
     </div>
   );
 }
